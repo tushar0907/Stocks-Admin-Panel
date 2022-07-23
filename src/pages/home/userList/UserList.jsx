4c14dc4da828a7c0a2e38aa6ea27bc856e1bd10c
Tushar <tusharkumar0807@gmail.com>
2022-07-23 18:33:34 +0530

    adding delete function

--- src/pages/home/userList/UserList.jsx
+++ src/pages/home/userList/UserList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./userList.css";
 import {DataGrid} from '@mui/x-data-grid';
 import {DeleteOutline} from "@material-ui/icons";
@@ -7,6 +7,11 @@ import {Link} from "react-router-dom";
 
 
 const UserList=() => {
+    const [data, setData]=useState(userRows);
+    const handleDelete=(id) => {
+        setData(data.filter((item) => item.id!==id))
+    }
+
     const columns=[
         {field: "id", headerName: "ID", width: 90},
         {
@@ -44,7 +49,7 @@ const UserList=() => {
                             <button className="userListEdit">Edit</button>
                         </Link>
 
-                        <DeleteOutline className="userListDelete" />
+                        <DeleteOutline className="userListDelete" onClick={() => handleDelete(params.row.id)} />
                     </>
                 )
             }
@@ -55,7 +60,7 @@ const UserList=() => {
     return (
         <div className="userList">
             <DataGrid
-                rows={userRows}
+                rows={data}
                 columns={columns}
                 disableSelectionOnClick
                 pageSize={4}
